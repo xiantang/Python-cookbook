2d0bc90ecb624302a088c1bb246f524e141fef37
zhujingdi <34479567+xiantang@users.noreply.github.com>
2018-07-23 23:41:34 +0800

    review 9.10-9.13
    review 9.10-9.13

--- 9-metaprogram/9.13.py
+++ 9-metaprogram/9.13.py
@@ -15,50 +15,50 @@
 # Spam.grok(42)
 # s = Spam()
 
-# class Singleton(type):
-#     def __init__(self,*args,**kwargs):
-#         self.__instance = None
-#         super().__init__(*args,**kwargs)
-#
-#     def __call__(self, *args, **kwargs):
-#         if self.__instance is None:
-#             self.__instance = super().__call__(*args,**kwargs)
-#             return self.__instance
-#         else:
-#             return self.__instance
-# class Spam(metaclass=Singleton):
-#
-#     def __init__(self):
-#         print("Creating Spam")
-#
-# a = Spam()
-# b = Spam()
-# print(a is b)
-
-import weakref
-
-class Cached(type):
+class Singleton(type):
     def __init__(self,*args,**kwargs):
+        self.__instance = None
         super().__init__(*args,**kwargs)
-        self.__cache = weakref.WeakValueDictionary()
 
     def __call__(self, *args, **kwargs):
-        if args in self.__cache:
-            return self.__cache[args]
+        if self.__instance is None:
+            self.__instance = super().__call__(*args,**kwargs)
+            return self.__instance
         else:
-            obj = super().__call__(*args)
-            self.__cache[args] = obj
-            return obj
+            return self.__instance
+class Spam(metaclass=Singleton):
 
-class Spam(metaclass=Cached):
-    def __init__(self, name):
-        print("Creating Spam({!r})".format(name))
-        self.name = name
+    def __init__(self):
+        print("Creating Spam")
+sp=Singleton("Spam",(),{}) #这是一个类
+a= sp()
+b= sp()
+print(a is b)
 
-
-spam = Cached("Spam",(),{"name" : "aaa"})()
-# print(spam.name)
-a = Spam('Guido')
-b = Spam('Diana')
-c = Spam('Guido')
-print(a is c)
+# import weakref
+#
+# class Cached(type):
+#     def __init__(self,*args,**kwargs):
+#         super().__init__(*args,**kwargs)
+#         self.__cache = weakref.WeakValueDictionary()
+#
+#     def __call__(self, *args, **kwargs):
+#         if args in self.__cache:
+#             return self.__cache[args]
+#         else:
+#             obj = super().__call__(*args)
+#             self.__cache[args] = obj
+#             return obj
+#
+# class Spam(metaclass=Cached):
+#     def __init__(self, name):
+#         print("Creating Spam({!r})".format(name))
+#         self.name = name
+#
+#
+# spam = Cached("Spam",(),{"name" : "aaa"})()
+# # print(spam.name)
+# a = Spam('Guido')
+# b = Spam('Diana')
+# c = Spam('Guido')
+# print(a is c)
